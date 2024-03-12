e56338dbe471eb2a3a7d7a41f7f15489fe25f562
Preston <prestonosoro56@gmail.com>
2024-03-12 14:55:54 +0300

    access element at index[0]

---
+++ array.js
@@ -0,0 +1,9 @@
+// function that takes an array containing only numbers and return the first element
+function getFirstValue(arr) {
+  return arr[0];
+}
+
+// Test cases
+console.log(getFirstValue([1, 2, 3])); // ➞ 1
+console.log(getFirstValue([80, 5, 100])); // ➞ 80
+console.log(getFirstValue([-500, 0, 50])); // ➞ -500
