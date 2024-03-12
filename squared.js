24d897ae239fe4193c5719f0f8bc7cb4dd678476
Preston <prestonosoro56@gmail.com>
2024-03-12 16:28:53 +0300

    correct syntax error

---
+++ squared.js
@@ -0,0 +1,9 @@
+//  Function to calculate the square of a number.
+function squared(b) {
+	return b ** 2
+}
+
+// Test cases
+console.log(squared(5)); //  ➞ 25
+console.log(squared(9)); // ➞ 81
+console.log(squared(100)); // ➞ 10000
