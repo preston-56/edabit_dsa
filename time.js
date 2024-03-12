c5a5e23930b892ff2b281f71beb3837a2bfbf4e9
Preston <prestonosoro56@gmail.com>
2024-03-12 14:03:30 +0300

    convert minutes to seconds

---
+++ time.js
@@ -0,0 +1,9 @@
+// Function to convert minutes to seconds
+function convert(minutes){
+    return minutes*60
+}
+
+// Test cases
+console.log(convert(5)); //  ➞ 300
+console.log(convert(3)); // ➞ 180
+console.log(convert(2));  // ➞ 120
