5e5e740cfdd7f0383e6feb0dabfbe740b8ce2bca
Preston <prestonosoro56@gmail.com>
2024-03-12 17:58:03 +0300

    concatenate string

---
+++ concatEdabit.js
@@ -0,0 +1,10 @@
+// Function to concatenate a passed string name with string "Edabit" and store it in a variable called result.
+function nameString(name) {
+  var result = name + "Edabit";
+  return result;
+}
+
+// Test cases
+console.log(nameString("Mubashir")); // ➞ "MubashirEdabit"
+console.log(nameString("Matt")); // ➞ "MattEdabit"
+console.log(nameString("javaScript")); // ➞ "javaScriptEdabit"
