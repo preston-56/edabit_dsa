59038e3bb79297b71cfdc088f0f41e4436b44028
Preston <prestonosoro56@gmail.com>
2024-03-12 17:10:57 +0300

    find the perimeter of a rectangle

---
+++ perimeter.js
@@ -0,0 +1,9 @@
+// Function to find the perimeter of a rectange.
+function findPerimeter(length, width) {
+  return (length + width) * 2;
+}
+
+// Test cases
+console.log(findPerimeter(6, 7)); // ➞ 26
+console.log(findPerimeter(20, 10)); // ➞ 60
+console.log(findPerimeter(2, 9)); // ➞ 22
