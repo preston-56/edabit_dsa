50a8deee3ed1f97b30d2f316f6c9859b0d99b71d
Preston <prestonosoro56@gmail.com>
2024-03-12 15:11:44 +0300

    correct syntax error

--- cube.js
+++ cube.js
@@ -1,9 +1,8 @@
-
-function cubes(a){
-    return a**3
+function cubes(a) {
+  return a ** 3; // corrected syntax
 }
 
 // Test cases
-console.log(cubes(3)) // ➞ 27
+console.log(cubes(3)); // ➞ 27
 console.log(cubes(5)); // ➞ 125
 console.log(cubes(10)); // ➞ 1000
