895bf99c591b3c035593ec2e2a1764e447b7b043
Preston <prestonosoro56@gmail.com>
2024-03-12 15:13:00 +0300

    increment integer by +1

---
+++ num.js
@@ -0,0 +1,9 @@
+// Function that takes a number as an argument, then increments it by +1 and returns the result.
+function addition(num){
+    return num + 1
+}
+
+// Test cases
+console.log(addition(0)); // ➞ 1
+console.log(addition(9)); // ➞ 10
+console.log(addition(-3)); // ➞ -2
