221868e34a3d58225ca3fe09a159e27bfcf4a11d
Preston <prestonosoro56@gmail.com>
2024-03-12 17:30:18 +0300

    check if number is less than or equal to zero

---
+++ numCheck.js
@@ -0,0 +1,9 @@
+// Function to check if a number is less than or equal to zero.
+function lessThanOrEqualToZero(num) {
+  return num <= 0;
+}
+
+// Test cases
+console.log(lessThanOrEqualToZero(5)); // ➞ false
+console.log(lessThanOrEqualToZero(0)); // ➞ true
+console.log(lessThanOrEqualToZero(-3)); // ➞ true
