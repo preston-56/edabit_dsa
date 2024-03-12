08368e3ad6c01e3a162ec42edf748c7493d6543b
Preston <prestonosoro56@gmail.com>
2024-03-12 13:42:05 +0300

    sum

--- sum.js
+++ sum.js
@@ -1,7 +1,9 @@
+// function to return the sum of two numbers
 function addition(a, b) {
   return a + b;
 }
 
-console.log(addition(3, 2));
-
-
+// Test cases
+console.log(addition(3, 2)); // ➞ 5
+console.log(addition(-3, -6)); // ➞ -9
+console.log(addition(7, 3)); // ➞ 10
