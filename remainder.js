9ffeb92adab305ce10acad255e4eaf2f499a2a54
Preston <prestonosoro56@gmail.com>
2024-03-12 16:45:53 +0300

    return remainder

---
+++ remainder.js
@@ -0,0 +1,10 @@
+// Function to get the remainder after dividing the fist parameter with the second parameter
+function remainder(x, y) {
+  return x % y;
+}
+
+// Test cases
+console.log(remainder(1, 3)); // ➞ 1
+console.log(remainder(3, 4)); // ➞ 3
+console.log(remainder(-9, 45)); // ➞ -9
+console.log(remainder(5, 5)); // ➞ 0
