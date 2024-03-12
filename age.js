a48d1863e2e60914080179a9285638f0dccdbc6d
Preston <prestonosoro56@gmail.com>
2024-03-12 15:21:07 +0300

    conver age in years to days

---
+++ age.js
@@ -0,0 +1,9 @@
+// Function that takes age n years and returns the age in days.
+function calcAge(age){
+    return age * 365
+}
+
+// Test cases
+console.log(calcAge(65)); // ➞ 23 725
+console.log(calcAge(0)); // ➞ 0
+console.log(calcAge(20)); // ➞ 7 300
