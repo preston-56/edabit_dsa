d83abdfeeb5c74ce38c3eb3305ecb5e77596810a
Preston <prestonosoro56@gmail.com>
2024-03-13 10:39:14 +0300

    sum check

---
+++ sumCheck.js
@@ -0,0 +1,9 @@
+// Function to check if the sum of two elements is less than 100.
+function lessThan100(a, b) {
+  return a + b < 100;
+}
+
+// Test cases
+console.log(lessThan100(22, 15)); //  ➞  true
+console.log(lessThan100(83, 34)); //  ➞ false
+console.log(lessThan100(3, 77)); //  ➞ true
